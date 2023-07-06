b5790b8898848b3c0e078a95475a7a92a0920d7a
Federico Luzzi <fedeluzzi00@gmail.com>
2023-07-06 11:02:51 +0100

    chore: update discord link

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,2 @@
-export const DISCORD_LINK = "https://discord.com/CarrotEth";
+export const DISCORD_LINK = "https://discord.gg/zCFXBEQkVa";
 export const TWITTER_LINK = "https://twitter.com/CarrotEth";
