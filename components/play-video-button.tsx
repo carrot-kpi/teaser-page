b34c89bc488153f2da3dcedb3609d30cb530d7fa
Paolo Guerra <pg.paologuerra@gmail.com>
2023-07-11 14:56:32 +0200

    fix: add playsinline to video, pause on close

--- components/play-video-button.tsx
+++ components/play-video-button.tsx
@@ -2,7 +2,7 @@
 
 import PlayVideo from "@/icons/play-video";
 import { Button, Modal } from "@/lib/carrot-kpi-ui";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 interface PlayVideoButtonProps {
     playVideoMessage: string;
@@ -13,6 +13,7 @@ function PlayVideoButton({
     playVideoMessage,
     videoNotSupportedMessage,
 }: PlayVideoButtonProps) {
+    const videoRef = useRef<HTMLVideoElement>(null);
     const [showVideo, setShowVideo] = useState(false);
 
     const handleClick = useCallback(() => {
@@ -21,13 +22,20 @@ function PlayVideoButton({
 
     const handleDismiss = useCallback(() => {
         setShowVideo(false);
+        if (!videoRef || !videoRef.current) return;
+        videoRef.current.pause();
     }, []);
 
     return (
         <>
             <Modal open={showVideo} onDismiss={handleDismiss}>
                 <div className="w-full md:w-1/2 aspect-video rounded-xl bg-gray-500">
-                    <video controls className="w-full h-full rounded-xl">
+                    <video
+                        ref={videoRef}
+                        controls
+                        playsInline
+                        className="aspect-video w-full border border-black rounded-xl bg-gray-500 overflow-hidden"
+                    >
                         <source
                             src="https://d2l3j8l4t44bvz.cloudfront.net"
                             type="video/webm"
